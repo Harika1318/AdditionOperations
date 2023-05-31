d61913abd9014dd1fd6f8af6b33e72035ddbe76d
harika1318 <kolisettiharika@gmail.com>
2023-05-31 15:02:59 +0530

    addition

---
+++ src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1=20;
+		int num2=30;
+		int result;
+		result=num1+num2;
+		
+		System.out.println("addition of two numbers"+" "+result);
+	}
+
+}
